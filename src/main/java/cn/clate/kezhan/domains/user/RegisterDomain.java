62f8f5e22d7b6c8e1269325caf592e97fb2a6812
Nora0713 <1379026619@qq.com>
2018-03-14 12:21:04 +0800

    add:register

---
+++ src/main/java/cn/clate/kezhan/domains/user/RegisterDomain.java
@@ -0,0 +1,29 @@
+package cn.clate.kezhan.domains.user;
+
+import cn.clate.kezhan.pojos.User;
+import cn.clate.kezhan.utils.Ret;
+import cn.clate.kezhan.utils.Tools;
+import cn.clate.kezhan.utils.factories.DaoFactory;
+import org.nutz.dao.Cnd;
+import org.nutz.dao.Dao;
+import org.nutz.dao.util.cri.SqlExpressionGroup;
+import org.nutz.lang.util.NutMap;
+
+public class RegisterDomain {
+
+    public static NutMap fire(String username, String password, String phone) {
+        Dao dao = DaoFactory.get();
+        User user = dao.fetch(User.class, username);
+        if (user != null) {
+            return Ret.e(450,"用户名已存在");
+        }
+        user = new User();
+        String pwEncrypted = Tools.passwordEncrypt(password);
+        user.setUsername(username);
+        user.setPassword(pwEncrypted);
+        user.setPhone(phone);
+        dao.insert(user);
+        return null;
+
+    }
+}
--- src/main/java/cn/clate/kezhan/modules/UserModule.java
+++ src/main/java/cn/clate/kezhan/modules/UserModule.java
@@ -1,6 +1,7 @@
 package cn.clate.kezhan.modules;
 
 import cn.clate.kezhan.domains.user.LoginDomain;
+import cn.clate.kezhan.domains.user.RegisterDomain;
 import cn.clate.kezhan.utils.Ret;
 import cn.clate.kezhan.utils.validators.SimpleValidator;
 import org.nutz.lang.util.NutMap;
@@ -27,4 +28,20 @@ public class UserModule {
     }
 
 
+    @At("/register")
+    @Ok("json")
+    public NutMap register(@Param("username") String username, @Param("password") String password,@Param("phone")String phone){
+        SimpleValidator validator = new SimpleValidator();
+        validator.now(username, "用户名").require().chsDash().lenMin(5).lenMax(16);
+        validator.now(password, "密码").require().lenMin(8).lenMax(40);
+        validator.now(phone, "手机号码").require().length(11);//我手机号用mobilephone验证通不过
+        if(!validator.check()){
+            return Ret.e(1, validator.getError());
+        }
+        NutMap ret = RegisterDomain.fire(username,password,phone);
+        if(ret != null){
+            return Ret.e(2, "注册失败");
+        }
+        return Ret.s(ret.addv("用户名",username));
+    }
 }
