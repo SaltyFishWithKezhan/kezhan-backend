20c034f0060bfad3748f53c0dd1b2f828ea0f506
jcboy111 <786197140@qq.com>
2018-03-25 20:32:49 +0800

    fix

---
+++ src/main/java/cn/clate/kezhan/pojos/Test.java
@@ -0,0 +1,32 @@
+package cn.clate.kezhan.pojos;
+
+import org.nutz.dao.entity.annotation.Column;
+import org.nutz.dao.entity.annotation.Id;
+import org.nutz.dao.entity.annotation.Table;
+
+/**
+ * Created by 蛟川小盆友 on 2018/3/23.
+ */
+@Table("test")
+public class Test {
+    @Id
+    private int id;
+    @Column("name")
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
