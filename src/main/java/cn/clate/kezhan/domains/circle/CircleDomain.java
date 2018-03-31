0be377878ed41d34fcafc23d218325c601964420
Nora0713 <1379026619@qq.com>
2018-03-31 20:35:20 +0800

    update:circle&type

--- src/main/java/cn/clate/kezhan/domains/circle/CircleDomain.java
+++ src/main/java/cn/clate/kezhan/domains/circle/CircleDomain.java
@@ -10,16 +10,24 @@ import org.nutz.dao.sql.Criteria;
 import org.nutz.lang.util.NutMap;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class CircleDomain {
 
-    public static NutMap getCirclesByPage(int pageNumber,int pageSize) {
+    public static NutMap getCirclesByTypePage(int type, int pageNumber, int pageSize) {
         Dao dao = DaoFactory.get();
         Pager pager = dao.createPager(pageNumber, pageSize);
-        List<Circle> circles = dao.query(Circle.class,  Cnd.where("status", "!=", -1).desc("id"),pager );  //LinkedList
-        pager.setRecordCount(dao.count(Circle.class,  Cnd.where("status", "!=", -1)));
-        if(circles==null)//TODO:  circles.size()==0
+        List<Circle> circles = new LinkedList<>();
+        if(type==0){
+            circles = dao.query(Circle.class, Cnd.where("status", "!=", -1).
+                    desc("id"), pager);
+        }else {
+            circles = dao.query(Circle.class, Cnd.where("status", "!=", -1).
+                    and("type_id","=",type).desc("id"), pager);
+        }
+        pager.setRecordCount(dao.count(Circle.class, Cnd.where("status", "!=", -1)));
+        if (circles == null)//TODO:  circles.size()==0
             return null;
         ArrayList<Circle> circleArrayList = new ArrayList<>(circles);
         NutMap ret = new NutMap();
@@ -27,7 +35,7 @@ public class CircleDomain {
         ret.addv("per_page_size", pager.getPageSize());
         ret.addv("page_count", pager.getPageCount());
         ret.addv("content", circleArrayList);
-        return Ret.s(ret);
+        return ret;
     }
 
 }
