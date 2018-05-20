7ccd260ad12f72fe0a0a7348bba9710bb6e43e0f
Nora0713 <1379026619@qq.com>
2018-05-21 00:01:19 +0800

    add:nei4j user && relation :green_heart:

--- src/main/java/cn/clate/kezhan/neo4j/demains/UserDomain.java
+++ src/main/java/cn/clate/kezhan/neo4j/demains/UserDomain.java
@@ -1,22 +1,51 @@
 package cn.clate.kezhan.neo4j.demains;
 
 import cn.clate.kezhan.neo4j.driver.Neo4jDriver;
+import cn.clate.kezhan.neo4j.pojos.User;
 import org.neo4j.driver.v1.*;
 
 import static org.neo4j.driver.v1.Values.parameters;
 
 public class UserDomain {
 
-    public static void  printGreeting(final String message) {
+
+    private final static String ADD_USER = "CREATE (u:User) " +
+            "SET u.username = $username , u.phone = $phone , u.realName = $realName " +
+            "RETURN u.username + ', from node ' + id(u)";
+
+    private final static String ADD_FRIEND_RELATIONSHIP = "MATCH (u1:User) WHERE u1.username = $username1 " +
+            "MATCH (u2:User) WHERE u2.username = $username2 " +
+            "WITH u1, u2 " +
+            "CREATE (u1)-[f:FRIEND]->(u2) " +
+            "SET f.startUser = $startUser, f.endUser = $endUser " +
+            "RETURN id(f) + ', from node ' + id(f)";
+
+    public static void addUser(User user) {
+        try (Session session = Neo4jDriver.getInstance().session()) {
+            String ret = session.writeTransaction(new TransactionWork<String>() {
+                @Override
+                public String execute(Transaction tx) {
+
+                    StatementResult result = tx.run(ADD_USER,
+                            parameters("username",user.getUsername(), "phone",user.getPhone(),
+                                    "realName",user.getRealName()));
+                    return result.single().get(0).asString();
+                }
+            });
+            System.out.println(ret);
+        }
+    }
+
+    public static void addFriendRs(User user1,User user2) {
         try (Session session = Neo4jDriver.getInstance().session()) {
             String greeting = session.writeTransaction(new TransactionWork<String>() {
                 @Override
                 public String execute(Transaction tx) {
-                    StatementResult result = tx.run("CREATE (a:Greeting) " +
-                                    "SET a.message = $message " +
-                                    "RETURN a.message + ', from node ' + id(a)",
-                            parameters("message", message));
-                    return result.single().get(0).asString();
+
+                    StatementResult result = tx.run(ADD_FRIEND_RELATIONSHIP,
+                            parameters("username1", user1.getUsername(), "username2", user2.getUsername(),
+                                    "startUser",user1.getUsername(), "endUser",user2.getUsername()));
+                    return result.next().get(0).asString();
                 }
             });
             System.out.println(greeting);
@@ -24,6 +53,14 @@ public class UserDomain {
     }
 
     public static void main(String... args) throws Exception {
-        printGreeting("hello, world");
+        User user = new User();
+        user.setRealName("胡狗子").setUsername("hhw").setPhone("137****4233");
+        addUser(user);
+        User user1 = new User();
+        User user2 = new User();
+        user1.setUsername("cyj");
+        user2.setUsername("hhw");
+        addFriendRs(user1,user2);
     }
+
 }
--- src/main/java/cn/clate/kezhan/neo4j/pojos/User.java
+++ src/main/java/cn/clate/kezhan/neo4j/pojos/User.java
@@ -1,28 +1,58 @@
 package cn.clate.kezhan.neo4j.pojos;
 
 
+import java.util.Date;
 import java.util.Set;
 
 public class User {
 
-    private Long id;
+    private int id;
 
     private String username;
 
+    private String password;
+
+    private int type;
+
+    private int gender;
+
+    private Date birthday;
+
+    private String avatar;
+
+    private String college;
+
+    private String className;
+
+    private String stuId;
+
     private String realName;
+
+    private String signature;
+
+    private long nowLogin;
+
+    private String nowLoginIp;
+
+    private long lastLogin;
+
+    private String lastLoginIp;
+
+    private long loginTimes;
 
     private String phone;
 
-    private int status;
+    private int lastActiveTime;
 
+    private String accessToken;
 
     Set<User> friends;
 
-    public Long getId() {
+    public int getId() {
         return id;
     }
 
-    public User setId(Long id) {
+    public User setId(int id) {
         this.id = id;
         return this;
     }
@@ -36,12 +66,138 @@ public class User {
         return this;
     }
 
+    public String getPassword() {
+        return password;
+    }
+
+    public User setPassword(String password) {
+        this.password = password;
+        return this;
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    public User setType(int type) {
+        this.type = type;
+        return this;
+    }
+
+    public int getGender() {
+        return gender;
+    }
+
+    public User setGender(int gender) {
+        this.gender = gender;
+        return this;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public User setBirthday(Date birthday) {
+        this.birthday = birthday;
+        return this;
+    }
+
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public User setAvatar(String avatar) {
+        this.avatar = avatar;
+        return this;
+    }
+
+    public String getCollege() {
+        return college;
+    }
+
+    public User setCollege(String college) {
+        this.college = college;
+        return this;
+    }
+
+    public String getClassName() {
+        return className;
+    }
+
+    public User setClassName(String className) {
+        this.className = className;
+        return this;
+    }
+
+    public String getStuId() {
+        return stuId;
+    }
+
+    public User setStuId(String stuId) {
+        this.stuId = stuId;
+        return this;
+    }
+
     public String getRealName() {
         return realName;
     }
 
     public User setRealName(String realName) {
         this.realName = realName;
+        return this;
+    }
+
+    public String getSignature() {
+        return signature;
+    }
+
+    public User setSignature(String signature) {
+        this.signature = signature;
+        return this;
+    }
+
+    public long getNowLogin() {
+        return nowLogin;
+    }
+
+    public User setNowLogin(long nowLogin) {
+        this.nowLogin = nowLogin;
+        return this;
+    }
+
+    public String getNowLoginIp() {
+        return nowLoginIp;
+    }
+
+    public User setNowLoginIp(String nowLoginIp) {
+        this.nowLoginIp = nowLoginIp;
+        return this;
+    }
+
+    public long getLastLogin() {
+        return lastLogin;
+    }
+
+    public User setLastLogin(long lastLogin) {
+        this.lastLogin = lastLogin;
+        return this;
+    }
+
+    public String getLastLoginIp() {
+        return lastLoginIp;
+    }
+
+    public User setLastLoginIp(String lastLoginIp) {
+        this.lastLoginIp = lastLoginIp;
+        return this;
+    }
+
+    public long getLoginTimes() {
+        return loginTimes;
+    }
+
+    public User setLoginTimes(long loginTimes) {
+        this.loginTimes = loginTimes;
         return this;
     }
 
@@ -54,12 +210,21 @@ public class User {
         return this;
     }
 
-    public int getStatus() {
-        return status;
+    public int getLastActiveTime() {
+        return lastActiveTime;
     }
 
-    public User setStatus(int status) {
-        this.status = status;
+    public User setLastActiveTime(int lastActiveTime) {
+        this.lastActiveTime = lastActiveTime;
+        return this;
+    }
+
+    public String getAccessToken() {
+        return accessToken;
+    }
+
+    public User setAccessToken(String accessToken) {
+        this.accessToken = accessToken;
         return this;
     }
 
